4c36a8f6f5945f64a9c6c043c3b2a6b9d142228d
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2024-07-24 20:27:05 +0530

    Template folder added...

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/Page')
+@app.route('/page')
 def Page():
     return render_template('index.html')
     # return 'Hello, World!'
